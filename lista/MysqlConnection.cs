22c00abe434a4e58e66629b46ed9fd02ef00d827
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-01-29 12:01:39 -0300

    Lista 29/01/24

---
+++ lista/MysqlConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace lista
+{
+    internal class MysqlConnection
+    {
+        private string conexao;
+
+        public MysqlConnection(string conexao)
+        {
+            this.conexao = conexao;
+        }
+    }
+}
---
+++ lista/Program.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*string[] lista = { "Erick", "Renon", "Richard", "Monique", "Samuel", "Gustavo", "Lucas", "Pedro", "Gui", "Edilson" };
+
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.ReadLine();*/
+
+            string conexao = "server=localhost;user=root;password=;database=comidas";
+
+            using (MySqlConnection banco = new MySqlConnection(conexao))
+            {
+                banco.Open();
+
+                string query = "SELECT * FROM refeicao";
+
+                using (MySqlCommand comando = new MySqlCommand(query, banco))
+                {
+                    using (MySqlDataReader reader = comando.ExecuteReader())
+                    {
+                        reader.Read();
+
+                        Console.WriteLine(reader["ID"]);
+                        Console.WriteLine(reader["Nome"]);
+                        Console.WriteLine(reader["Tipo"]);
+                        Console.WriteLine(reader["Temperatura"]);
+                    }
+
+                }
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
