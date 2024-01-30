38c0c8b968e2b04c9a5834cfa661be8df4cb7f1a
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-01-30 11:59:34 -0300

    Bd Windows.Form 30/01/24

---
+++ Bd/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Bd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbnome = new System.Windows.Forms.Label();
+            this.lbtipo = new System.Windows.Forms.Label();
+            this.lbtemp = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbnome
+            // 
+            this.lbnome.AutoSize = true;
+            this.lbnome.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lbnome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbnome.Location = new System.Drawing.Point(452, 63);
+            this.lbnome.Name = "lbnome";
+            this.lbnome.Size = new System.Drawing.Size(66, 24);
+            this.lbnome.TabIndex = 0;
+            this.lbnome.Text = "Nome";
+            // 
+            // lbtipo
+            // 
+            this.lbtipo.AutoSize = true;
+            this.lbtipo.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lbtipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtipo.Location = new System.Drawing.Point(452, 111);
+            this.lbtipo.Name = "lbtipo";
+            this.lbtipo.Size = new System.Drawing.Size(52, 24);
+            this.lbtipo.TabIndex = 1;
+            this.lbtipo.Text = "Tipo";
+            // 
+            // lbtemp
+            // 
+            this.lbtemp.AutoSize = true;
+            this.lbtemp.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.lbtemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbtemp.Location = new System.Drawing.Point(421, 160);
+            this.lbtemp.Name = "lbtemp";
+            this.lbtemp.Size = new System.Drawing.Size(129, 24);
+            this.lbtemp.TabIndex = 2;
+            this.lbtemp.Text = "Temperatura";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(556, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(556, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(556, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(182, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(638, 244);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(100, 51);
+            this.btnConsultar.TabIndex = 6;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriar.Location = new System.Drawing.Point(499, 244);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(100, 51);
+            this.btnCriar.TabIndex = 7;
+            this.btnCriar.Text = "Criar ";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.btnCriar_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(10, 52);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(399, 299);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.btnCriar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbtemp);
+            this.Controls.Add(this.lbtipo);
+            this.Controls.Add(this.lbnome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbnome;
+        private System.Windows.Forms.Label lbtipo;
+        private System.Windows.Forms.Label lbtemp;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Bd/Form1.cs
@@ -0,0 +1,107 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string conexaoString = "server=localhost;user=root;password=;database=comidas;";
+
+        public void criar(String informacao)
+        {
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                string Nome = "teste Nome";
+                string Tipo = "teste Tipo";
+                string Temperatura = "teste Temperatura";
+
+
+                conexao.Open();
+                string insert_informacao = "INNSERT INTO refeicao (Nome,Tipo,Temperatura) VALUE (@Nome, @Tipo, @Temperatura)";
+
+                using (MySqlCommand comando = new MySqlCommand(insert_informacao, conexao))
+                {
+                    comando.Parameters.AddWithValue("@Nome", Nome);
+                    comando.Parameters.AddWithValue("@Tipo", Tipo);
+                    comando.Parameters.AddWithValue("@Temperatura", Temperatura);
+
+                    comando.ExecuteNonQuery();
+                }
+       
+            }
+            
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e)
+        {
+            string nome = "", tipo = "",
+                temp = "";
+
+
+            nome = textBox1.Text;
+            tipo = textBox2.Text;
+            temp = textBox3.Text;
+
+                using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+                {
+                    string Nome = "teste Nome";
+                    string Tipo = "teste Tipo";
+                    string Temperatura = "teste Temperatura";
+
+
+                    conexao.Open();
+                    string insert_informacao = "INNSERT INTO refeicao (Nome,Tipo,Temperatura) VALUE (@Nome, @Tipo, @Temperatura)";
+
+                    using (MySqlCommand comando = new MySqlCommand(insert_informacao, conexao))
+                    {
+                        comando.Parameters.AddWithValue("@Nome", Nome);
+                        comando.Parameters.AddWithValue("@Tipo", Tipo);
+                        comando.Parameters.AddWithValue("@Temperatura", Temperatura);
+
+                        comando.ExecuteNonQuery();
+                    }
+
+                }
+
+            }
+        
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                string Nome = "teste Nome";
+                string Tipo = "teste Tipo";
+                string Temperatura = "teste Temperatura";
+
+
+                conexao.Open();
+                string insert_alternativa = "INNSERT INTO refeicao (Nome,Tipo,Temperatura) VALUE (@Nome, @Tipo, @Temperatura)";
+
+                using (MySqlCommand comando = new MySqlCommand(insert_alternativa, conexao))
+                {
+                    comando.Parameters.AddWithValue("@Nome", Nome);
+                    comando.Parameters.AddWithValue("@Tipo", Tipo);
+                    comando.Parameters.AddWithValue("@Temperatura", Temperatura);
+
+                    comando.ExecuteNonQuery();
+                }
+
+            }
+           
+        }
+    }
+}
