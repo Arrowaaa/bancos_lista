9150507bc928b53749594348f310347a4c475ca1
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-01-30 11:14:15 -0300

    30/01/24

--- lista/Program.cs
+++ lista/Program.cs
@@ -1,30 +1,88 @@
 ﻿using MySql.Data.MySqlClient;
-using Mysqlx.Connection;
+using Mysqlx.Crud;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lista
+namespace bancoDados
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string conexao =
+                "server=localhost;user=root;password=;database=comida";
 
-            /*string[] lista = { "Erick", "Renon", "Richard", "Monique", "Samuel", "Gustavo", "Lucas", "Pedro", "Gui", "Edilson" };
-
-            for (int i = 0; i < lista.Length; i++)
+            while (true)
             {
-                Console.WriteLine(i);
-                Console.WriteLine(lista[i]);
+                Console.WriteLine("1-Consultar");
+                Console.WriteLine("2-Criar");
+                Console.WriteLine("3-Sair");
+                Console.WriteLine("Escolha uma opção:");
+                string escolha = Console.ReadLine();
+
+                if (escolha == "1")
+                {
+                    ConsultarBancoDados(conexao);
+                }
+                else if (escolha == "2")
+                {
+                    cadastrarBancoDados(conexao);
+                }
+                else if (escolha == "3")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("escolha uma opção correta!");
+                }
+
+
             }
 
-            Console.ReadLine();*/
 
-            string conexao = "server=localhost;user=root;password=;database=comidas";
+            //Console.ReadKey();
+        }
 
+        private static void cadastrarBancoDados(string conexao)
+        {
+            using (MySqlConnection banco = new MySqlConnection(conexao))
+            {
+                banco.Open();
+                try
+                {
+
+                    string Nome = "teste Nome";
+                    string Tipo = "teste Tipo";
+                    string Temperatura = "teste Temperatura";
+
+
+
+                    //codigo bonitão..
+                    banco.Open();
+                    string query = "INSERT INTO comidas(Nome, Tipo, Temperatura) VALUE (@nome, @tipo, @temperatura);";
+
+                    using (MySqlCommand comando = new MySqlCommand(query,banco)) 
+                    {
+                        comando.Parameters.AddWithValue("@nome", Nome);
+                        comando.Parameters.AddWithValue("@tipo", Tipo);
+                        comando.Parameters.AddWithValue("@Temperatura", Temperatura);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+        private static void ConsultarBancoDados(string conexao)
+        {
             using (MySqlConnection banco = new MySqlConnection(conexao))
             {
                 banco.Open();
@@ -35,21 +93,20 @@ namespace lista
                 {
                     using (MySqlDataReader reader = comando.ExecuteReader())
                     {
-                        reader.Read();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["id"]);
+                            Console.WriteLine(reader["Nome"]);
+                            Console.WriteLine(reader["Tipo"]);
+                            Console.WriteLine(reader["Temperatura"]);
+                        }
 
-                        Console.WriteLine(reader["ID"]);
-                        Console.WriteLine(reader["Nome"]);
-                        Console.WriteLine(reader["Tipo"]);
-                        Console.WriteLine(reader["Temperatura"]);
+
                     }
 
                 }
+
             }
-
-
-
-
-            Console.ReadLine();
         }
     }
 }
